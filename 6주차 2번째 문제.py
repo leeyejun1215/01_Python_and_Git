ffe24f2d04629ef20eae6f254dc189afb4b3b5a1
leeyejun1215 <happytok2@naver.com>
2024-10-24 21:02:02 +0900

    Update 6주차 2번째 문제.py

--- 6주차 2번째 문제.py
+++ 6주차 2번째 문제.py
@@ -5,7 +5,6 @@ class person:
         self.age = age
         self.contact = contact
     def info(self):
-        print(f"회원님의 전화번호는{self.name} 나이는 {self.age} 연락처는 {self.contact} 입니다.")
         print(f"회원님의 이름은 {self.name} 나이는 {self.age} 연락처는 {self.contact} 입니다.")
 
 name = input("이름을 입력하세요:")
